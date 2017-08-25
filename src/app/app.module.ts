a8e5ed36c6dcb623e73f83496554dc3ce26369a9
Kartika Prasad <pure.zero.23@gmail.com>
2017-08-25 21:50:04 +1200

    That's a start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdInputModule} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdInputModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
